ba89388b2a7436e7f64c6036bd17aa1766cae51d
Marcio <marciocravomoreira@gmail.com>
2019-08-25 18:07:47 +0100

    Add feature

---
+++ BookingChallengeProject/BookingProject.feature
@@ -0,0 +1,15 @@
+﻿Feature: BookingProject
+	In order to book an hotel reservation
+	As a traveller in my holidays
+	I want to select hotels using the filters
+
+Scenario: Book hotel using filter is completed successfully
+	Given I am in the booking website
+		And The booking page is opened
+		And I enter the location of the hotel
+		And I select the reservation dates
+		And I select the reservation for 2 people
+		And I select only 1 room in this reservation
+	When The hotel search is completed
+	And I select the recommended for you filter of Sauna
+	Then I find in the list the hotel name Limerick Strand Hotel 
---
+++ BookingChallengeProject/BookingProjectSteps.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace BookingChallengeProject
+{
+    [Binding]
+    public class BookingProjectSteps
+    {
+        [Given(@"I am in the booking website")]
+        public void GivenIAmInTheBookingWebsite()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"The booking page is opened")]
+        public void GivenTheBookingPageIsOpened()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"I enter the location of the hotel")]
+        public void GivenIEnterTheLocationOfTheHotel()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"I select the reservation dates")]
+        public void GivenISelectTheReservationDates()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"I select the reservation for (.*) people")]
+        public void GivenISelectTheReservationForPeople(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"I select only (.*) room in this reservation")]
+        public void GivenISelectOnlyRoomInThisReservation(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"The hotel search is completed")]
+        public void WhenTheHotelSearchIsCompleted()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [When(@"I select the recommended for you filter of Sauna")]
+        public void WhenISelectTheRecommendedForYouFilterOfSauna()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"I find in the list the hotel name Limerick Strand Hotel")]
+        public void ThenIFindInTheListTheHotelNameLimerickStrandHotel()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+    }
+}
