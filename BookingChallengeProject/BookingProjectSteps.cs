fb95466ebb55210df16ffa6399e42b2eff1c77d2
Marcio <marciocravomoreira@gmail.com>
2019-08-26 18:09:47 +0100

    Add feature change

--- BookingChallengeProject/BookingProjectSteps.cs
+++ BookingChallengeProject/BookingProjectSteps.cs
@@ -1,18 +1,22 @@
 ﻿using System;
+using System.Diagnostics;
 using Xunit;
 using Xunit.Extensions;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Support;
+using OpenQA.Selenium.Support.UI;
 using TechTalk.SpecFlow;
+using System.Globalization;
+using OpenQA.Selenium.Support.PageObjects;
 
 namespace BookingChallengeProject
 {
     [Binding]
     public class BookingProjectSteps
     {
+        private const string Message = "month";
         private IWebDriver _driver;
-        private string expectedSearchFieldTip;
 
         [Given(@"I am in the booking website")]
         public void GivenIAmInTheBookingWebsite()
@@ -24,22 +28,49 @@ namespace BookingChallengeProject
         [Given(@"The booking page is successfully opened")]
         public void GivenTheBookingPageIsSuccessfullyOpened()
         {
-            expectedSearchFieldTip = "Search";
-            IWebElement searchFieldId = _driver.FindElement(By.Id("ss"));
-            string actualSearchFieldTip = searchFieldId.GetProperty("placeholder");
+            var expectedSearchFieldTip = "Search";
+            IWebElement searchButton = _driver.FindElement(By.CssSelector(".sb-searchbox__button"));
+            var actualSearchFieldTip = searchButton.Text;
             Assert.Equal(expectedSearchFieldTip, actualSearchFieldTip);
         }
 
         [Given(@"I enter the location of the hotel")]
         public void GivenIEnterTheLocationOfTheHotel()
         {
-            //ScenarioContext.Current.Pending();
+            IWebElement searchArgument = _driver.FindElement(By.Id("ss"));
+            searchArgument.SendKeys("Limerick County, Irlanda");
         }
 
         [Given(@"I select the reservation dates")]
         public void GivenISelectTheReservationDates()
         {
-            //ScenarioContext.Current.Pending();
+            DateTime todaysDatePlus3Months = DateTime.Now.Date.AddMonths(+3);
+            string reservationMonth = todaysDatePlus3Months.ToString("MMMM");
+            string reservationDay = todaysDatePlus3Months.Day.ToString();
+
+            IWebElement checkInDate = _driver.FindElement(By.CssSelector(".sb-date-field.b-datepicker"));
+            checkInDate.Click();
+
+            System.Collections.Generic.IList<IWebElement> calendarMonths = _driver.FindElements(By.CssSelector(".bui-calendar__month"));
+            foreach (IWebElement month in calendarMonths)
+            {
+                if (month.Text != reservationMonth)
+                {
+                    IWebElement calenderBoxNext = _driver.FindElement(By.CssSelector(".bui-calendar__control.bui-calendar__control--next"));
+                    calenderBoxNext.Click();
+                    break;
+                }
+            }
+
+            System.Collections.Generic.IList<IWebElement> calendarDays = _driver.FindElements(By.CssSelector(".bui-calendar__date"));
+            foreach (IWebElement day in calendarDays)
+            {
+                if ( day.Text == reservationDay)
+                {
+                    day.Click();
+                    break;
+                }
+            }
         }
 
         [Given(@"I select the reservation for (.*) people")]
